d4d1a56e9afce111df8fd6b54d41ecb7f7830338
AshThrax <78923290+AshThrax@users.noreply.github.com>
2023-11-12 23:21:49 +0100

    implementation de la methode Create Owner

--- GraphQlApitest/GraphQlApitest/Models/OwnerConsumer.cs
+++ GraphQlApitest/GraphQlApitest/Models/OwnerConsumer.cs
@@ -36,5 +36,44 @@ namespace GraphQlApitest.Models
             var response = await _client.SendQueryAsync<ResponseOwnerCollectionType>(query);
             return response.Data.Owners;
         }
+
+        public async Task<Owner> GetOwner(Guid Id)
+        {
+            var query = new GraphQLRequest {
+                Query = @"
+                query ownersQuery($ownerID: ID!){
+
+                    owners(ownerId: $ownerID){
+                      id
+                      name 
+                      address 
+                      account{
+                               id 
+                               type 
+                               description 
+                             }    
+                           }    
+                        }",
+                //permet d'intergrer des parametre dans le querry
+                Variables= new {ownerID=Id }
+            };
+            //attente de la requeste
+            var response = await _client.SendQueryAsync<ResponseOwnerType>(query);
+            return response.Data.Owner;
+        }
+
+        public async Task<Owner> CreateOwner(Ownerinput ownerToCreate)
+        {
+            var query = new GraphQLRequest
+            {
+                Query = @"mutation($owner: ownerInput!){ createOwner(owner: $owner){ id,name,address} }",
+                //ajout des parametre relatif au querry
+                Variables = new {owner=ownerToCreate}
+            };
+
+            var response =await _client.SendMutationAsync <ResponseOwnerType>(query);
+            return response.Data.Owner;
+        }
+
     }
 }
