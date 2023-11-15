c01de0f2283a852d3ce0fbd496a209abac6d6b37
AshThrax <78923290+AshThrax@users.noreply.github.com>
2023-11-15 11:33:05 +0100

    resolution des dependance lié a graphql

---
+++ GraphQlApitest/Graphql_ASpNetCore_Medium_Tutoriel/ExtensionMethods/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using Graphql_ASpNetCore_Medium_Tutoriel.GraphQl;
+using Graphql_ASpNetCore_Medium_Tutoriel.Model;
+using Graphql_ASpNetCore_Medium_Tutoriel.Service;
+
+namespace Graphql_ASpNetCore_Medium_Tutoriel.ExtensionMethods
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddgraphQl
+            (this IServiceCollection services)
+        {
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<EmployeeDetailsType>();
+            services.AddScoped<EmployeeQuery>();
+            services.AddScoped<ISchema, EmployeeDetailsSchema>();
+            services.AddGraphQL(x => x.AddAutoSchema<EmployeeQuery>()//schema 
+                                            .AddSystemTextJson());//serializer
+            return services;
+        }
+    }
+}
--- GraphQlApitest/Graphql_ASpNetCore_Medium_Tutoriel/Program.cs
+++ GraphQlApitest/Graphql_ASpNetCore_Medium_Tutoriel/Program.cs
@@ -1,3 +1,6 @@
+using GraphQL.Types;
+using Graphql_ASpNetCore_Medium_Tutoriel.ExtensionMethods;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,14 +9,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//injection des service lié a graphQl
+builder.Services.AddgraphQl();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   
+app.UseGraphQL<ISchema>("/graphql");//url pour l'host du endpoint graph ql
+app.UseGraphQLPlayground(
+    "/",// url pour host playground
+    new GraphQL.Server.Ui.Playground.PlaygroundOptions 
+    { 
+        GraphQLEndPoint="/graphql",//url endpoint graphql
+        SubscriptionsEndPoint="/graphql"//url endpoint graphql
+    }
+    );
 }
 
 app.UseHttpsRedirection();
